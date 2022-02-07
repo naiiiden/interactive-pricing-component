a0dc07cfb46ed2740355ae25210610a33f5da316
naiden <na4ko359@gmail.com>
2022-02-07 19:18:53 +0200

    desktop design completed/added hover effects

--- script/script.js
+++ script/script.js
@@ -81,10 +81,3 @@ inputYearly.oninput = function(){
     }
 };
 inputYearly.oninput();
-
-
-// - 10K pageviews / $8 per month
-// - 50K pageviews / $12 per month
-// - 100K pageviews / $16 per month
-// - 500k pageviews / $24 per month
-// - 1M pageviews / $36 per month
